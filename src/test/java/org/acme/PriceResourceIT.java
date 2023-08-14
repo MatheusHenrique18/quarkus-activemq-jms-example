431f4028e1978dd98a69ddfe90bde3a14ba17ad0
MatheusHenrique18 <matheus.hsb18@outlook.com>
2023-08-13 23:54:15 -0300

    subindo exemplo simples de mensageria com activemq, utilizando o broker artemis via docker image.

---
+++ src/test/java/org/acme/PriceResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class PriceResourceIT extends PriceResourceTest {
+    // Execute the same tests but in packaged mode.
+}
